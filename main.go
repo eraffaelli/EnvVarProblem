21824e8d5f8b64e47f5a13cc7de1e748729b173c
Emilien Raffaëlli <emilienraffaelli@hotmail.com>
2019-06-04 15:11:13 +0200

    Init commit

---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var (
+	RedisHost string
+	RedisPort string
+)
+
+func init() {
+	if RedisHost = os.Getenv("REDIS_HOST"); RedisHost == "" {
+		RedisHost = "localhost"
+	}
+	if RedisPort = os.Getenv("REDIS_PORT"); RedisPort == "" {
+		RedisPort = "6379"
+	}
+
+	println("T1")
+	temp := RedisHost + `:` + RedisPort
+	println(temp)
+	println()
+	println("T2")
+	temp2 := RedisHost + `:` + RedisPort
+	println(temp2)
+	println()
+	println("T3")
+	temp3 := RedisHost + RedisPort
+	println(temp3)
+	println()
+	println("Variable :")
+	println(RedisHost)
+	println(RedisPort)
+	println()
+	println("Sprintf :")
+	println(fmt.Sprintf("RedisHost RedisPort %s:%s\n", RedisHost, RedisPort))
+	println(fmt.Sprintf("RedisHost RedisPort %s : %s\n", RedisHost, RedisPort))
+	portInt, _ := strconv.Atoi(RedisPort)
+	println()
+	println("Atoi :\n")
+	println(portInt)
+	print("From os.getenv\n")
+	println(fmt.Sprintf(
+		"%s:%s\n",
+		os.Getenv("REDIS_HOST"),
+		os.Getenv("REDIS_PORT"),
+	))
+	fmt.Printf("RedisHost : %+v\n", RedisHost)
+	println("")
+	fmt.Printf("RedisPort : %+v\n", RedisPort)
+	fmt.Printf("%T\n", RedisHost)
+	fmt.Printf("%T\n", RedisPort)
+}
+
+func main() {
+
+}
